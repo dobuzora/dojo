1ced056127172dc6f6ddb23354e0ce6205369bb9
cat2koban <minamiedamame0328@gmail.com>
2019-07-30 20:10:41 +0900

    ミニマムなコンポーネントの作成

--- vue/cat2koban/vue-alarm/src/App.vue
+++ vue/cat2koban/vue-alarm/src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App!!" />
+    <HelloWorld value="Hello Vue.js"></HelloWorld>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import Vue from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
 
-@Component({
+export default Vue.extend({
+  name: "App",
   components: {
     HelloWorld
   }
-})
-export default class App extends Vue {}
+});
 </script>
 
 <style>
@@ -23,7 +22,10 @@ export default class App extends Vue {}
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+}
+body {
+  color: #DDDDDD;
+  background-color: #333333;
 }
 </style>
